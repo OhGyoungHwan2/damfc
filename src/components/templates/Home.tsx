7b7d3769cc71b7b8c78eea8a4353d9c84c821ba4
경환오 <rudghks531@naver.com>
2024-04-22 18:21:51 +0900

    [fix] Select Tab value 적용 toUpperCase()

--- src/components/templates/Home.tsx
+++ src/components/templates/Home.tsx
@@ -126,7 +126,7 @@ const Home: React.FC<{
         ])
       );
       return {
-        value: key,
+        value: key.toUpperCase(),
         node: <Stadium positionItems={positionItems} />,
       };
     });
