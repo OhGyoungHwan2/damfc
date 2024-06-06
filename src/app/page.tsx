571a9a8c1fa5ffb228267535d90c5827f4ec1b36
경환오 <rudghks531@naver.com>
2024-06-06 12:54:10 +0900

    [fix] get ranker버그 해결

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Home from "../components/templates/Home";
-import { TGETRanker } from "./api/ranker/route";
+// import { TGETRanker } from "./api/ranker/route";
 import { TGETRecommend } from "./api/recommend/route";
 import type { Metadata } from "next";
 
@@ -19,25 +19,25 @@ async function getRecommend() {
   return res.json();
 }
 
-async function getRanker() {
-  const res = await fetch(`${process.env.BASE_URL}/api/ranker`, {
-    cache: "force-cache",
-  });
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
-  return res.json();
-}
+// async function getRanker() {
+//   const res = await fetch(`${process.env.BASE_URL}/api/ranker`, {
+//     cache: "force-cache",
+//   });
+//   if (!res.ok) {
+//     throw new Error("Failed to fetch data");
+//   }
+//   return res.json();
+// }
 
 export default async function Page() {
   // 데이터
   const responseRecommend = (await getRecommend()) as TGETRecommend;
-  const responseRanker = (await getRanker()) as TGETRanker;
+  // const responseRanker = (await getRanker()) as TGETRanker;
 
   return (
     <Home
       responseRecommend={responseRecommend}
-      responseRanker={responseRanker}
+      // responseRanker={responseRanker}
     />
   );
 }
