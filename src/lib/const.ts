ae6d45217353661e6c31b40340cc69f4ad996b84
경환오 <rudghks531@naver.com>
2024-09-26 15:46:41 +0900

    [add] 새로운 시즌, 팀컬러 변동사항 적용

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -168,8 +168,8 @@ export const adaptability = {
 export const enhanceTeamcolor = {
   "동빛 물결 LV.1": { all: 1 },
   "은빛 물결 LV.1": { all: 1 },
-  "은빛 물결 LV.2": { all: 2 },
-  "금빛 물결 LV.1": { all: 2 },
+  "은빛 물결 LV.2": { all: 3 },
+  "금빛 물결 LV.1": { all: 3 },
   "금빛 물결 LV.2": { all: 4 },
 };
 
