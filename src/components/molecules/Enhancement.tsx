6148ae92bb8eb25a636c3248ce39868ae519c3e5
unknown <rudghks531@naver.com>
2024-12-11 16:05:21 +0900

    [patch] 정기 업데이트 팀컬러오류해결

--- src/components/molecules/Enhancement.tsx
+++ src/components/molecules/Enhancement.tsx
@@ -14,7 +14,7 @@ const Enhancement: React.FC<EnhancementProps> = ({
     if (enhance <= 4) return "bg-[#ad5f42] text-[#7e3f27]";
     if (enhance <= 7) return "bg-[#b8bdca] text-[#4e545e]";
     if (enhance <= 10) return "bg-[#dca908] text-[#695100]";
-    if (enhance <= 13) return "bg-gradient-to-r from-red-600 from-10% via-neutral-50 via-50% to-sky-600 to-90%";
+    if (enhance <= 13) return "bg-gradient-to-r from-red-600 from-3% via-neutral-50 via-50% to-sky-600 to-97% text-[#4e545e]";
     return "";
   };
 
